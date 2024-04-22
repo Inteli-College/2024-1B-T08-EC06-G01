d96ba5c52eb0608a8dceea37c407d29613af2e83
GustavoWidman (Arch) <gustavo.widman@sou.inteli.edu.br>
2024-04-22 17:15:01 -0300

    chore: Initialize some sprint pages and fix site overall

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -35,7 +35,7 @@ const config: Config = {
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: './sidebars.js',
+          sidebarPath: './sidebars.ts',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           routeBasePath: '/',
