4ec6a2fce0b5c6b74c87a283b95b5bae2d4ea9ed
GustavoWidman (laptop-arch) <gustavo.widman@sou.inteli.edu.br>
2024-05-08 17:52:14 -0300

    desenvolvimentos da CLI 08/05
    Co-authored-by: Rizzi <marco.meneguetti@sou.inteli.edu.br>
    Co-authored-by: Rodrigo <rodrigo.santos@sou.inteli.edu.br>

--- src/meu_workspace/src/cannabot/cannabot/cannabot.py
+++ src/meu_workspace/src/cannabot/cannabot/cannabot.py
@@ -1,77 +1,37 @@
 import inquirer
 import typer
 import rclpy
-from rclpy.node import Node
-from geometry_msgs.msg import Twist
-
-class Robot(Node):
-    def __init__(self):
-        super().__init__('robot')
-        self.publisher = self.create_publisher(
-            msg_type=Twist,
-            topic='/turtle1/cmd_vel',
-            qos_profile=10
-        )
-        timer_period = 0.5
-        self.timer = self.create_timer(
-            timer_period_sec=timer_period, 
-            callback=self.timer_callback
-            )
-        
-    def timer_callback(self):
-        msg = Twist()
-        msg.linear.x = 0.5
-        msg.angular.z = 0.5
-        self.publisher.publish(msg)
-        # self.get_logger().info("Publicando mensagem de movimento")
+from classes.robot import TurtleBot
 
 app = typer.Typer()
 
-def moviment_publisher():
-    pass
-
-def front():
-    print("Move to the front of the line")
-
-def back():
-    print("Move to the back of the line")
-
-def left():
-    graus_left = inquirer.text("Quantos graus para esquerda deseja virar?")
-    print(f"Move to the left in {graus_left} degrees")
-
-def right():
-    graus_right = inquirer.text("Quantos graus para direita deseja virar?")
-    print(f"Move to the right in {graus_right} degrees")
-
 def show_menu():
-    questions = [inquirer.List('action', message="What do you want to do?", choices=['front', 'back', 'left', 'right', 'exit'])]
-    answers = inquirer.prompt(questions)
-    return answers['action']
+    return inquirer.prompt(
+        inquirer.List('action', message="What do you want to do?", choices=[
+            'front', 'back', 'left', 'right', 'exit'
+        ]
+    ))['action'] # type: ignore
 
-def init_robot(args=None):
-    print("Init robot")
-    # rclpy.init(args=args)
-    # robot = Robot()
-    # rclpy.spin(robot)
-    # robot.destroy_node()
-    # rclpy.shutdown()
 
 def main():
-    init_robot()
-
+    rclpy.init(args=None)
+    robot = TurtleBot()
     while True:
         action = show_menu()
-        if action == 'front':
-            front()
-        elif action == 'back':
-            back()
-        elif action == 'left':
-            left()
-        elif action == 'right':
-            right()
-        elif action == 'exit':
-            break
+        match action:
+            case 'front':
+                robot.move_forward(0.1)
+            case 'back':
+                robot.move_backward(0.1)
+            case 'left':
+                robot.rotate_left(0.1)
+            case 'right':
+                robot.rotate_right(0.1)
+            case 'exit':
+                break
+
+    robot.destroy_node()
+    rclpy.shutdown()
 
 if __name__ == "__main__":
     main()
