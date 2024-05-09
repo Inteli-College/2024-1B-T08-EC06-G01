67993e908500802295b6ce762fea4cdc5c7321a9
Rodrigo-07 <rodrigo.santos@sou.inteli.edu.br>
2024-05-09 11:16:32 -0300

    Classe do robô para o código da CLI para testar movimentação

--- src/meu_workspace/src/cannabot/cannabot/cannabot.py
+++ src/meu_workspace/src/cannabot/cannabot/cannabot.py
@@ -1,16 +1,22 @@
 import inquirer
 import typer
 import rclpy
-from classes.robot import TurtleBot
+from rclpy.node import Node
+from geometry_msgs.msg import Twist, Vector3
+from std_msgs.msg import String
+import time
+
+# from classes.robot import TurtleBot
 
 app = typer.Typer()
 
 def show_menu():
-    return inquirer.prompt(
+    questions = [
         inquirer.List('action', message="What do you want to do?", choices=[
             'front', 'back', 'left', 'right', 'exit'
-        ]
-    ))['action'] # type: ignore
+        ])
+    ]
+    return inquirer.prompt(questions)['action']
 
 
 def main():
@@ -20,14 +26,19 @@ def main():
         action = show_menu()
         match action:
             case 'front':
-                robot.move_forward(0.1)
+                print("Mover para frente")
+                robot.move_forward(0.1, 1.0)
             case 'back':
-                robot.move_backward(0.1)
+                print("Mover para trás")
+                robot.move_backward(0.1, 1.0)
             case 'left':
-                robot.rotate_left(0.1)
+                print("Mover para a esquerda")
+                robot.rotate_left(2.0, 1.0)
             case 'right':
-                robot.rotate_right(0.1)
+                print("Mover para a direita")
+                robot.rotate_right(2.0, 1.0)
             case 'exit':
+                print("Exit")
                 break
 
     robot.destroy_node()
@@ -35,3 +46,40 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+class TurtleBot(Node):
+    def __init__(self):
+        super().__init__('turtlebot')
+
+        self.publisher_ = self.create_publisher(Twist, 'cmd_vel', 10)
+
+    # Função para enviar comandos de movimento para o robô
+    def move(self, linear: Vector3, angular: Vector3, duration: float):
+        msg = Twist()
+        msg.linear = linear
+        msg.angular = angular
+        self.publisher_.publish(msg)
+
+        # Esperar o tempo de duração antes de para o movimento
+        time.sleep(duration)  # Simples delay para esperar antes de parar o movimento
+        self.stop()
+    
+    # 
+    def stop(self):
+        stop_msg = Twist()
+        self.publisher_.publish(stop_msg)
+
+    def move_forward(self, speed: float, duration: float):
+        # Criar um Vector3 com a velocidade linear
+        self.move(Vector3(x=speed, y=0.0, z=0.0), Vector3(), duration)
+
+    def move_backward(self, speed: float, duration: float):
+        self.move(Vector3(x=-speed, y=0.0, z=0.0), Vector3(), duration)
+
+    def rotate_left(self, speed: float, duration: float):
+        # Criar um Vector3 com a velocidade angular
+        self.move(Vector3(), Vector3(z=speed), duration)
+
+    def rotate_right(self, speed: float, duration: float):
+        self.move(Vector3(), Vector3(z=-speed), duration)
