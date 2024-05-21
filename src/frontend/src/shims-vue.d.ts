f49bac7449fbba8ea841c6a89a82d2b213770967
Gustavo Widman <123963822+GustavoWidman@users.noreply.github.com>
2024-05-21 18:00:11 -0300

    Revert "Feat/frontend"

--- src/frontend/src/shims-vue.d.ts
+++
@@ -1,7 +0,0 @@
-// Esse arquivo tem o objetivo de ajudar o TypeScript a entender arquivos .vue 
-
-declare module '*.vue' {
-    import type { DefineComponent } from 'vue';
-    const component: DefineComponent<{}, {}, any>;
-    export default component;
-  }
