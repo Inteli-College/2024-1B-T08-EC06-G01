899e622f4f5903006df97b1df8313ee4d146ded7
GustavoWidman (laptop-arch) <gustavo.widman@sou.inteli.edu.br>
2024-05-23 10:54:53 -0300

    feat: reinvent the wheel (fix camera)

---
+++ src/package/camera/main.py
@@ -0,0 +1,72 @@
+import asyncio
+import base64
+import json
+
+import cv2
+import websockets
+
+
+class WebSocketServer:
+    def __init__(self, host='localhost', port=8765):
+        self.host = host
+        self.port = port
+        self.clients = set()
+        self.loop = None
+        self.thread = None
+        self.capture = cv2.VideoCapture(0)
+
+    async def register_client(self, websocket):
+        self.clients.add(websocket)
+        try:
+            async for msg in websocket:
+                pass  # Ignore incoming messages
+        finally:
+            self.clients.remove(websocket)
+
+    async def _broadcast(self, message):
+        if self.clients:
+            await asyncio.gather(*[client.send(message) for client in self.clients])
+
+    def broadcast(self, message):
+        """Synchronous wrapper for the asynchronous _broadcast function."""
+        if self.loop:
+            asyncio.run_coroutine_threadsafe(self._broadcast(message), self.loop)
+
+    async def websocket_handler(self, websocket, path):
+        await self.register_client(websocket)
+
+    def start_server(self):
+        self.loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(self.loop)
+        server_coro = websockets.serve(self.websocket_handler, self.host, self.port)
+        self.loop.run_until_complete(server_coro)
+
+        self.loop.create_task(self.broadcast_image_forever())
+
+        print(f"WebSocket server started on ws://{self.host}:{self.port}")
+        self.loop.run_forever()
+
+    # def run_in_thread(self):
+    #     self.thread = threading.Thread(target=self.start_server)
+    #     self.thread.start()
+
+    async def broadcast_image(self):
+        ret, frame = self.capture.read()
+        if ret:
+            _, buffer = cv2.imencode('.jpg', frame)
+            self.broadcast(json.dumps({
+                "bytes": base64.b64encode(buffer).decode('utf-8')
+            }))
+
+    async def broadcast_image_forever(self):
+        while not self.loop:
+            await asyncio.sleep(0.1)
+
+        while True:
+            self.loop.create_task(self.broadcast_image())
+            await asyncio.sleep(0.02) # 50Hz
+
+if __name__ == "__main__":
+    websocket_server = WebSocketServer(host='0.0.0.0')
+    websocket_server.start_server()
+
--- src/package/run_camera.sh
+++ src/package/run_camera.sh
@@ -11,16 +11,7 @@ source env/bin/activate
 echo "Installing dependencies..."
 pip install -r requirements.txt > /dev/null
 
-cd workspace
+cd camera
 
-# get the site-packages path by getting pip show setuptools (setuptools always comes with pip)
-VENV_PATH=$(pip show setuptools | grep "Location: " | awk '{print $2}')
-
-export PYTHONPATH="$PYTHONPATH:$VENV_PATH"
-
-echo "Building package..."
-colcon build > /dev/null
-
-source install/setup.bash
-
-ROS_DOMAIN_ID=69 ros2 run cannabot_camera camera
+# ROS_DOMAIN_ID=69 ros2 run cannabot_camera camera
+python3 main.py
