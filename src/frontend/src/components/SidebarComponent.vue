51168e5e18e2b59bf878e05d717a1ca235d85cb3
laurapbueno <laura.bueno@sou.inteli.edu.br>
2024-06-05 16:04:52 -0300

    Ajustes front

--- src/frontend/src/components/SidebarComponent.vue
+++ src/frontend/src/components/SidebarComponent.vue
@@ -1,7 +1,32 @@
 <template>
-  <div class="sidebar" :style="{ width: sidebarWidth }">
-    <div class="whitecanna">
-      <WhiteCanna />
+  <div class="sidebar" 
+  :class="{ open: !collapsed }" 
+  :style="{ width: sidebarWidth }" 
+  @mouseenter="openSidebar"
+  @mouseleave="closeSidebar"
+  >
+    <div class="whitecanna" :class="{ open: !collapsed }">
+      <button class="whitecanna" @click="goToHome">
+        <svg width="53" height="42" viewBox="0 0 53 42" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M0 11.1008C5.31857 15.1487 17.8105 25.9724 25.2297 36.884C24.7263 21.9375 15.435 9.68091 0 11.1008Z" fill="url(#paint0_linear_177_161)"/>
+        <path d="M41.4585 1.44849C40.1775 8.33931 36.0542 25.0554 29.8085 36.7933C42.2631 30.2618 48.8921 16.1307 41.4585 1.44849Z" fill="url(#paint1_linear_177_161)"/>
+        <path d="M18.772 0C21.3561 6.45021 26.7901 22.7232 27.8532 36.2133C34.6387 23.1765 32.6274 7.48409 18.772 0Z" fill="url(#paint2_linear_177_161)"/>
+        <defs>
+        <linearGradient id="paint0_linear_177_161" x1="5.48109" y1="7.48188" x2="24.996" y2="37.0383" gradientUnits="userSpaceOnUse">
+        <stop stop-color="white"/>
+        <stop offset="1" stop-color="#999999"/>
+        </linearGradient>
+        <linearGradient id="paint1_linear_177_161" x1="46.736" y1="5.40484" x2="25.5811" y2="33.6242" gradientUnits="userSpaceOnUse">
+        <stop stop-color="white"/>
+        <stop offset="1" stop-color="#999999"/>
+        </linearGradient>
+        <linearGradient id="paint2_linear_177_161" x1="25.1957" y1="0" x2="25.1957" y2="36.2133" gradientUnits="userSpaceOnUse">
+        <stop stop-color="white"/>
+        <stop offset="1" stop-color="#999999"/>
+        </linearGradient>
+        </defs>
+        </svg>
+      </button>
     </div>
     <div :class="['line', { 'line-collapsed': collapsed }]"></div>
     <div class="nav">
@@ -45,18 +70,34 @@ export default defineComponent({
 
     function goToControl() {
       router.push('/control');
+      toggleSidebar();
     }
 
     function goToRegister() {
       router.push('/register');
+      toggleSidebar();
     }
 
     function goToHome() {
       router.push('/home');
+      toggleSidebar();
     }
 
     function goToLogin(){
       router.push('/login')
+      toggleSidebar();
+    }
+
+    function openSidebar() {
+      if (collapsed.value) {
+        toggleSidebar();
+      }
+    }
+
+    function closeSidebar() {
+      if (!collapsed.value) {
+        toggleSidebar();
+      }
     }
 
     return {
@@ -67,7 +108,9 @@ export default defineComponent({
       goToRegister,
       goToHome,
       goToLogin,
-      WhiteCanna
+      WhiteCanna,
+      openSidebar,
+      closeSidebar
     };
   }
 });
@@ -81,6 +124,38 @@ export default defineComponent({
 </style>
 
 <style scoped>
+:root {
+  --sidebar-bg-color: #077336;
+  --sidebar-item-hover: #C3C1C1;
+}
+
+.sidebar {
+  color: #077336;
+  background-color: var(--sidebar-bg-color);
+  float: left;
+  position: fixed;
+  z-index: 1;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  padding: 0.5em;
+  transition: 0.4s ease;
+  display: flex;
+  flex-direction: column;
+}
+
+.sidebar.open .whitecanna {
+  flex-grow: 1
+}
+
+.whitecanna {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  margin-bottom: 0.5rem;
+}
+
 .line {
   height: 0.12rem;
   background-color: white;
@@ -121,25 +196,27 @@ export default defineComponent({
   transform: rotate(180deg);
   transition: 0.2s linear;
 }
- .out-button{
+
+.out-button{
   color: black;
   border: none;
   background: transparent;
   display: flex;
   align-items: center;
-  padding: 0.75em;
+  padding: 0.8em;
   margin: 0.25em ;
   cursor: pointer;
   white-space: nowrap; 
   margin-top: 24em;
+  margin-bottom: 2em;
+}
 
- }
-
- .button-text {
+.button-text {
   margin-left: 0.5em;
   font-size: 1.1em;
   font-weight:bold;
 }
+
 .nav-button {
   color: white;
   border: none;
