f04f3c61abd574f2fb57bf7b3a7ca91e4c4206a3
LuizFernandoLeao <luiz.leao@sou.inteli.edu.br>
2024-06-12 16:20:09 -0300

    feat: separação tabelas location e temp, e início da criação da paginação

--- src/frontend/src/views/Home.vue
+++ src/frontend/src/views/Home.vue
@@ -28,8 +28,6 @@
             <th>Data</th>
             <th>Hora</th>
             <th>Localização</th>
-            <th>Temperatura</th>
-            <th>Uso de IA</th>
           </tr>
         </thead>
         <tbody>
@@ -37,11 +35,95 @@
             <td>{{ item.date }}</td>
             <td>{{ item.time }}</td>
             <td>{{ item.location }}</td>
-            <td>{{ item.temp }}°C</td>
-            <td>{{ item.ia_usage }}</td>
           </tr>
         </tbody>
       </table>
+      <nav class="pagination">
+        <ul class="flex items-center -space-x-px h-10 text-base">
+          <li>
+            <a href="#" class="flex items-center justify-center px-4 h-10 leading-tight text-black bg-green-400 border border-green-300 hover:bg-green-100 hover:text-gray-700">
+              <span class="sr-only">Previous</span>
+              <svg class="w-3 h-3 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
+              </svg>
+            </a>
+          </li>
+          <li>
+            <a href="#" class="flex items-center justify-center px-4 h-10 leading-tight text-black bg-green-300 border border-green-200 hover:bg-green-100 hover:text-gray-700">1</a>
+          </li>
+          <li>
+            <a href="#" class="flex items-center justify-center px-4 h-10 leading-tight text-black bg-green-400 border border-green-300 hover:bg-green-100 hover:text-gray-700">2</a>
+          </li>
+          <li>
+            <a href="#" aria-current="page" class="flex items-center justify-center px-4 h-10 leading-tight text-black bg-green-400 border border-green-300 hover:bg-green-100 hover:text-gray-700">3</a>
+          </li>
+          <li>
+            <a href="#" class="flex items-center justify-center px-4 h-10 leading-tight text-black bg-green-400 border border-green-300 hover:bg-green-100 hover:text-gray-700">4</a>
+          </li>
+          <li>
+            <a href="#" class="flex items-center justify-center px-4 h-10 leading-tight text-black bg-green-400 border border-green-300 hover:bg-green-100 hover:text-gray-700">5</a>
+          </li>
+          <li>
+            <a href="#" class="flex items-center justify-center px-4 h-10 leading-tight text-black bg-green-400 border border-green-300 hover:bg-green-100 hover:text-gray-700">
+              <span class="sr-only">Next</span>
+              <svg class="w-3 h-3 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
+              </svg>
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <table class="table2">
+        <thead>
+          <tr>
+            <th>Data</th>
+            <th>Hora</th>
+            <th>Temperatura</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="item in items" :key="item.id">
+            <td>{{ item.date }}</td>
+            <td>{{ item.time }}</td>
+            <td>{{ item.temp }}°C</td>
+          </tr>
+        </tbody>
+      </table>
+      <nav class="pagination2">
+        <ul class="flex items-center -space-x-px h-10 text-base">
+          <li>
+            <a href="#" class="flex items-center justify-center px-4 h-10 leading-tight text-black bg-green-400 border border-green-300 hover:bg-green-100 hover:text-gray-700">
+              <span class="sr-only">Previous</span>
+              <svg class="w-3 h-3 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
+              </svg>
+            </a>
+          </li>
+          <li>
+            <a href="#" class="flex items-center justify-center px-4 h-10 leading-tight text-black bg-green-300 border border-green-200 hover:bg-green-100 hover:text-gray-700">1</a>
+          </li>
+          <li>
+            <a href="#" class="flex items-center justify-center px-4 h-10 leading-tight text-black bg-green-400 border border-green-300 hover:bg-green-100 hover:text-gray-700">2</a>
+          </li>
+          <li>
+            <a href="#" aria-current="page" class="flex items-center justify-center px-4 h-10 leading-tight text-black bg-green-400 border border-green-300 hover:bg-green-100 hover:text-gray-700">3</a>
+          </li>
+          <li>
+            <a href="#" class="flex items-center justify-center px-4 h-10 leading-tight text-black bg-green-400 border border-green-300 hover:bg-green-100 hover:text-gray-700">4</a>
+          </li>
+          <li>
+            <a href="#" class="flex items-center justify-center px-4 h-10 leading-tight text-black bg-green-400 border border-green-300 hover:bg-green-100 hover:text-gray-700">5</a>
+          </li>
+          <li>
+            <a href="#" class="flex items-center justify-center px-4 h-10 leading-tight text-black bg-green-400 border border-green-300 hover:bg-green-100 hover:text-gray-700">
+              <span class="sr-only">Next</span>
+              <svg class="w-3 h-3 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
+              </svg>
+            </a>
+          </li>
+        </ul>
+      </nav>
     </div>
   </div>
 </template>
@@ -237,4 +319,57 @@ onMounted(() => {
 .table tbody tr:nth-child(even) {
   height: 1.8rem;
 }
+
+.table2 {
+  margin-left: 55rem;
+  width: 44rem;
+  border-collapse: collapse;
+  margin-top: -7.1rem;
+  cursor: pointer;
+}
+
+.table2 th, .table2 td {
+  border: 0.06rem solid;
+  padding: 0.5rem;
+  padding-left: 1rem;
+  padding-right: 1rem;
+  text-align: center;
+  font-family: Poppins;
+  font-weight: normal;
+}
+
+.table2 th {
+  background-color: #ffffff;
+}
+
+.table2 tbody tr:nth-child(even) {
+  background-color: #ffffff;
+}
+
+.table2 tbody tr:hover {
+  background-color: #E4FFF0;
+}
+
+.table2 tbody tr:nth-child(odd) {
+  height: 1.8rem;
+}
+
+.table2 tbody tr:nth-child(even) {
+  height: 1.8rem;
+}
+
+.pagination {
+  margin-top: 2rem;
+  margin-left: 3rem;
+  display: flex;
+  justify-content: center;
+}
+
+.pagination2 {
+  margin-top: 2rem;
+  margin-left: 65rem;
+  display: flex;
+  justify-content: center;
+}
+
 </style>
